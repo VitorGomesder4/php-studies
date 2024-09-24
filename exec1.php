8fa87128c8e71a12dfcd412cbee7a6b29fc123c9
Vitor Gomes <vitor.araujo.derreteman4@gmail.com>
2024-09-24 14:31:27 -0300

    V2 do codigo que possui o uso de validação de caracteres especiais e isnumeric para segurança e evitar injeção maliciosa.

--- exec1.php
+++ exec1.php
@@ -11,11 +11,19 @@
         </form>
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST"){
-            $var1 = $_REQUEST["valor1"];
-            $var2 = $_REQUEST["valor2"];
-            $soma = $var1 + $var2;
-            echo "Soma:" . $soma . "<br />" ;
-            echo "Média" . (($var1 + $var2)/2); #no lugar de '$var1 + $var2' pode usar $soma
+            $var1 = htmlspecialchars($_REQUEST["valor1"]);
+            $var2 = htmlspecialchars($_REQUEST["valor2"]);
+            
+            if (isnumeric($var1) && is_numeric($var2)) 
+            {
+                $soma = $var1 + $var2;
+                echo "Soma:" . $soma . "<br />" ;
+                echo "Média" . (($var1 + $var2)/2); #no lugar de '$var1 + $var2' pode usar $soma
+
+            } else {
+                echo "Por favor, insira números válidos.";
+
+            }
         }
         
         ?>
